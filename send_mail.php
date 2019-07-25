d18cdac35102f4a9fa217f0b35fcf326b28cd070
Qi <qchen125@gmail.com>
2019-07-25 08:36:04 -0400

    changing to Contact.html

--- send_mail.php
+++ send_mail.php
@@ -9,7 +9,7 @@ $webmaster_email = "name@example.com";
 This bit sets the URLs of the supporting pages.
 If you change the names of any of the pages, you will need to change the values here.
 */
-$feedback_page = "feedback_form.html";
+$feedback_page = "Contact.html";
 $error_page = "error_message.html";
 $thankyou_page = "thank_you.html";
 
@@ -20,9 +20,9 @@ If you add a form field, you will need to add it here.
 $email_address = $_REQUEST['email_address'] ;
 $comments = $_REQUEST['comments'] ;
 $first_name = $_REQUEST['first_name'] ;
-$msg = 
-"First Name: " . $first_name . "\r\n" . 
-"Email: " . $email_address . "\r\n" . 
+$msg =
+"First Name: " . $first_name . "\r\n" .
+"Email: " . $email_address . "\r\n" .
 "Comments: " . $comments ;
 
 /*
@@ -58,7 +58,7 @@ elseif (empty($first_name) || empty($email_address)) {
 header( "Location: $error_page" );
 }
 
-/* 
+/*
 If email injection is detected, redirect to the error page.
 If you add a form field, you should add it here.
 */
