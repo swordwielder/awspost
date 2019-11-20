6b0283503e6cb07fc59e040d1d7a9c54f2383642
Qi <qchen125@gmail.com>
2019-11-20 15:03:16 -0500

    adding new website

---
+++ precache-manifest.fa695a3ea8a0c633f9c1cf53a7d20a51.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de63c9bea06d08ddce310a29e270f73b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a98ba11326a599f60199",
+    "url": "/static/css/2.48c3d4a3.chunk.css"
+  },
+  {
+    "revision": "9941751357faf6bf6c20",
+    "url": "/static/css/main.308ed943.chunk.css"
+  },
+  {
+    "revision": "a98ba11326a599f60199",
+    "url": "/static/js/2.8d154d47.chunk.js"
+  },
+  {
+    "revision": "9941751357faf6bf6c20",
+    "url": "/static/js/main.936045e7.chunk.js"
+  },
+  {
+    "revision": "c198fa1c6c88b9d6012e",
+    "url": "/static/js/runtime-main.debfa7ee.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
